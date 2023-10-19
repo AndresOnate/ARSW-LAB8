f79a86542eedc7f2ab95b46a2203c65d09978514
= <andres.onate@labinfo.is.escuelaing.edu.co>
2023-10-19 09:57:03 -0500

    Avance

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -36,8 +36,8 @@ var app = (function () {
         //subscribe to /topic/TOPICXX when connections succeed
         stompClient.connect({}, function (frame) {
             console.log('Connected: ' + frame);
-            stompClient.subscribe('/topic/TOPICXX', function (eventbody) {
-                
+            stompClient.subscribe('/topic/newpoint', function (eventbody) {
+                alert(JSON.parse(eventbody.body).x, (eventbody.body).y);
                 
             });
         });
@@ -59,6 +59,7 @@ var app = (function () {
             var pt=new Point(px,py);
             console.info("publishing point at "+pt);
             addPointToCanvas(pt);
+            stompClient.send("/topic/newpoint", {}, JSON.stringify(pt)); 
 
             //publicar el evento
         },
